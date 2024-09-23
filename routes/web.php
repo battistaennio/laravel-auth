5d6d4fc1bbec1a70214e450be3fdf2b2ec153fa0
battistaennio <ennio.battista99@gmail.com>
2024-09-23 15:49:44 +0200

    fix: vari

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [PageController::class, 'index'])->name('home');
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('edit');
