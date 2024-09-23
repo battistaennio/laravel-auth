9fd3701a81d7732655c748810bac33a915c595e2
battistaennio <ennio.battista99@gmail.com>
2024-09-23 18:19:59 +0200

    fix: add rutes admin.home, admin.projects.index

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,10 +1,10 @@
 <aside class="text-bg-dark">
     <ul class="lh-lg">
         <li>
-            <a href="#"><i class="fa-solid fa-house"></i> Home</a>
+            <a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a>
         </li>
         <li>
-            <a href="#"><i class="fa-solid fa-list"></i> Elenco Progetti</a>
+            <a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-list"></i> Elenco Progetti</a>
         </li>
         <li>
             <a href="#"><i class="fa-solid fa-plus"></i> Nuovo Progetto</a>
